9e85fe5d4c6676b8abae41dd412d120b69864bd5
sc-alexp <152201418+sc-alexp@users.noreply.github.com>
2024-02-05 09:58:57 -0600

    Create scripts.js

---
+++ website/scripts.js
@@ -0,0 +1,14 @@
+function readTextFile(file) { 
+  var rawFile = new XMLHttpRequest(); 
+  rawFile.open("GET", file, false); 
+  rawFile.onreadystatechange = function () { 
+    if (rawFile.readyState === 4) { 
+      if (rawFile.status === 200 || rawFile.status == 0) { 
+        var allText = rawFile.responseText; 
+          document.getElementById("text-container").innerHTML = allText; 
+        } 
+      } 
+    } 
+    rawFile.send(null); 
+  } 
+readTextFile("announcement.txt"); 
