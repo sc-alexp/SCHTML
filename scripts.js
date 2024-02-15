759ff25667a74e5af5d054dd084d0b6cfd806782
sc-alexp <152201418+sc-alexp@users.noreply.github.com>
2024-02-15 12:20:33 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -188,3 +188,16 @@ function color() {
                 alert('No text selected');
             }
 }
+
+// define a handler
+function doc_keyUp(e) {
+
+    // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+    if (e.ctrlKey && e.code === 'B') {
+        // call your function to do the thing
+        bold();
+    }
+}
+
+// register the handler 
+document.addEventListener('keyup', doc_keyUp, false);
